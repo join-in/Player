ef67bdf47389cd34d3dce8a778b95fd9a1ca5d21
chags002 <suresh.x.chaganti.-nd@disney.com>
2018-05-28 08:23:48 -0400

    Revert: Player Module initial cut (6bf86c6)

--- src/main/java/com/gameOrganiser/palyer/controller/PlayerController.java
+++ src/main/java/com/gameOrganiser/palyer/controller/PlayerController.java
@@ -9,8 +9,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 
